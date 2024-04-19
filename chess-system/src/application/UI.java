19f714fa5867c38163f5565e3f3415b5d1e1926c
Frouzin <matheusereis44@gmail.com>
2024-04-19 12:54:38 -0300

    Correção de bug na UI e Excepition created

--- chess-system/src/application/UI.java
+++ chess-system/src/application/UI.java
@@ -5,9 +5,9 @@ import chess.ChessPiece;
 public class UI {
 
     public static void printBoard(ChessPiece[][] pieces){
-        for( int i=0; i<pieces.length; i++){
-            System.out.println((8-i) + " ");
-            for (int j=0; j<pieces.length; j++){
+        for (int i = 0; i < pieces.length; i++) {
+            System.out.print((8 - i) + " ");
+            for (int j = 0; j < pieces[i].length; j++) {
                 printPiece(pieces[i][j]);
             }
             System.out.println();
@@ -17,10 +17,10 @@ public class UI {
 
     public static void printPiece(ChessPiece piece){
         if (piece == null){
-            System.out.println("-");
+            System.out.print("-");
         }else{
-            System.out.println(piece);
+            System.out.print(piece);
         }
-        System.out.println(" ");
+        System.out.print(" ");
     }
 }
